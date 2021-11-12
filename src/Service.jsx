171de9c941237d953634ce0b9d7d6b9d17629f50
Osama-14 <mohdosama42000@gmail.com>
2021-11-12 16:27:27 +0500

    ore-changes

--- src/Service.jsx
+++ src/Service.jsx
@@ -1,27 +1,25 @@
-import React from 'react'
+import React from "react";
+import Cards from "./Cards";
 
+const Service = () => {
+  return (
+    <>
+      <div className="my-5">
+        <h1 className="text-center">Our Services</h1>
+      </div>
+      <div className="container-fluid mb-5">
+        <div className="row">
+          <div className="col-10 max-auto">
+            <div className="row gy-4">
+              <div className="col-md-4 col-10 mx-auto">
+                  <Cards/>
 
-const Hom =() => {
-    return(
-        <>
-        <div className="my-5">
-            <h1 className="tab-center">Our Services</h1>
-        </div>
-        <div className="container-fluid mb-5">
-            <div className="row">
-                <div className="col-10 max-auto">
-
-                    <div className="row">
-
-                        <div className="col-md-4 col-10 mx-auto"></div>
-                        
-                         </div>
-
-                </div>
             </div>
+            </div>
+          </div>
         </div>
-         </>
-    );
-
+      </div>
+    </>
+  );
 };
-export default Hom;
+export default Service;
